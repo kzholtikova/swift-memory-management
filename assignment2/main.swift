083893f4aec7d9f13a9ee31df1f5af198e578d52
Kira Zholtikova <kzholtikova@kse.org.ua>
2024-10-10 20:08:40 +0300

    task 2

--- assignment2/main.swift
+++ assignment2/main.swift
@@ -54,3 +54,49 @@ person = nil
 // Weak and unowned ref to tenant in a Apartment both break the retain cycle.
 // Weak ref can hold nil => objects lifetimes doesn't interfere.
 // Unowned ref presumes a Person is valid from the onset and will outlive an Apartment.
+
+
+
+// MARK: - Task 2: Weak Nodes (4 points)
+
+class Node {
+    var value : Int
+    var children : [Node]
+    var neighbors: [weakNode]
+    
+    init(value: Int) {
+        self.value = value
+        self.children = []
+        self.neighbors = []
+    }
+    
+    deinit {
+        print("Node \(value) deinitialized")
+    }
+    
+    func addNeighbor(_ neighbor: Node) {
+        neighbors.append(weakNode(from: neighbor))
+    }
+}
+
+struct weakNode {
+    weak var node: Node?
+    
+    init(from node: Node) {
+        self.node = node
+    }
+}
+
+var node1 = Node(value: 1)
+var node2 = Node(value: 2)
+var node3 = Node(value: 3)
+
+node1.addNeighbor(node2)
+node2.addNeighbor(node3)
+node3.addNeighbor(node1)
+
+if isKnownUniquelyReferenced(&node1) && isKnownUniquelyReferenced(&node2) && isKnownUniquelyReferenced(&node3) {
+    print("\nReference counts aren't affected by neighbors.\n")
+} else {
+    print("\nThere're other strong references to the nodes!\n")
+}
