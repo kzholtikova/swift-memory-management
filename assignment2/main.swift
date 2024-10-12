9720acbfe98c12d422616375193efcc72abf0ad4
Kira Zholtikova <kzholtikova@kse.org.ua>
2024-10-12 10:09:17 +0300

    task 3

--- assignment2/main.swift
+++ assignment2/main.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 
-// MARK: - Task 1: Break the retain cycle (3 points)
+// MARK: - Task 1: Break the retain cycle
 class Apartment {
     let number: Int
     weak var tenant: Person?  // <- use `weak` here
@@ -57,7 +57,7 @@ person = nil
 
 
 
-// MARK: - Task 2: Weak Nodes (4 points)
+// MARK: - Task 2: Weak Nodes
 
 class Node {
     var value : Int
@@ -100,3 +100,72 @@ if isKnownUniquelyReferenced(&node1) && isKnownUniquelyReferenced(&node2) && isK
 } else {
     print("\nThere're other strong references to the nodes!\n")
 }
+
+
+
+// MARK: - Task 3: Custom copy on write
+
+class IntArray {  // wrapper class to ensure array is ref type
+    var array: [Int]
+    
+    init(array: [Int]) {
+        self.array = array
+    }
+}
+
+struct MyData {  // value type to disable auto CoW
+    private var data: IntArray
+
+    init(data: [Int] = []) {
+        self.data = IntArray(array: data)
+    }
+    
+    private mutating func copyOnWrite() {
+        print("Is the Int array uniquely referenced: \(isKnownUniquelyReferenced(&data))")
+        if !isKnownUniquelyReferenced(&data) {
+            data = IntArray(array: data.array)
+        }
+    }
+    
+    subscript(index: Int) -> Int {
+        get { return data.array[index] }
+        set {
+            copyOnWrite()
+            data.array[index] = newValue
+        }
+    }
+    
+    mutating func append(_ element: Int) {
+        copyOnWrite()
+        data.array.append(element)
+        print("Is the Int array uniquely referenced after mutating: \(isKnownUniquelyReferenced(&data))")
+    }
+    
+    mutating func remove(at index: Int) {
+        copyOnWrite()
+        data.array.remove(at: index)
+        print("Is the Int array uniquely referenced after mutating: \(isKnownUniquelyReferenced(&data))")
+    }
+    
+    func count() -> Int {
+        return data.array.count
+    }
+    
+    func printArray() {
+        print(data.array)
+    }
+}
+
+var numbers = MyData(data: [1, 2, 3])
+var numbers2 = numbers
+print("Initial array: ")
+numbers.printArray()
+
+numbers.append(4)
+print("Mutated array: ")
+numbers.printArray()
+print("Unchanged array: ")
+numbers2.printArray()
+print()
+
+
